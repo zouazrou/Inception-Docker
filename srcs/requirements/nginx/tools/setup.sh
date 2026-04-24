515d1a926f508baa7ddeffd6bb32954ac4f77770
Zakaria Wazrou <zakariawazrou@gmail.com>
2026-04-24 11:47:53 +0100

    Run nginx from script instead of run it from Dockefile

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -2,7 +2,12 @@
 
 mkdir -p /etc/nginx/ssl/
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout /etc/nginx/ssl/inception.key \
-    -out /etc/nginx/ssl/inception.crt \
-    -subj "/CN=${DOMAIN_NAME}/"
+if [ ! -f /etc/nginx/ssl/inception.crt ]; then
+    mkdir -p /etc/nginx/ssl/
+    openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+        -keyout /etc/nginx/ssl/inception.key \
+        -out /etc/nginx/ssl/inception.crt \
+        -subj "/CN=${DOMAIN_NAME}/"
+fi
+
+exec nginx -g "daemon off;"
