e48f5f9d348d1d267879b612df63b6dd83897dd1
zouazrou <zakariawazrou@gmail.com>
2026-05-04 11:37:38 +0100

    Completing script & Dockerfile

--- srcs/requirements/bonus/redis/tools/setup.sh
+++ srcs/requirements/bonus/redis/tools/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+sed 's/bind 127.0.0.1 -::1/bind 0.0.0.0/' /etc/redis/redis.conf -i
+sed 's/daemonize yes/daemonize no/' /etc/redis/redis.conf -i
 
-sed -i 's/bind 127.0.0.1/bind 0.0.0.0/g' /etc/redis/redis.conf
-cat << EOF >> /etc/redis/redis.conf
-maxmemory 256mb
-maxmemory-policy allkeys-lfu
-EOF
-
-exec redis-server /etc/redis/redis.conf --daemonize no --protected-mode no --requirepass "$WP_REDIS_PASSWORD"
+# automatically removes the least recently used keys (LRU)
+# when the maxmemory limit is reached
+echo "maxmemory 256mb" >> /etc/redis/redis.conf
+echo "maxmemory-policy allkeys-lru" >> /etc/redis/redis.conf
